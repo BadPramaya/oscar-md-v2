eecdd4a064cac5d222d56349f1d4f5e4e2207cb6
x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>
2022-08-14 21:44:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,31 @@
 // RECODE BY OSCAR OFC
 
 //=============『 Utama 』================== //
-global.owner = ['6285850539404','79609055704'] 
+global.owner = ['94770611075','94770611075'] 
 global.mods = [''] 
-global.prems = ['6285850539404'] 
+global.prems = ['94770611075'] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ᴹᴿ᭄ Jarot offcঔৣ꧂'
-global.numberowner = '6285850539404','79609055704',
-global.instagram = 'https://instagram.com/Jarotr_'
-global.github = 'https://github.com/Alpiii22'
-global.Youtube = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
-global.sc = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
-global.ss = 'https://youtu.be/3m7tRD4RklM'
-global.sss = 'https://youtu.be/PzUH1ApMCig'
-global.yt = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.nameowner = '©️x_ᴘʀᴀᴍᴀʏᴀ_x'
+global.numberowner = '94770611075','94770611075',
+global.instagram = 'https://instagram.com/x_pramaya_x'
+global.github = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.Youtube = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.sc = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.ss = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.sss = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.yt = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
 global.dana = '085850539404'
 global.pulsa = '085850539404'
 global.gopay = '085850539404'
 
 //=============『 Info Bot 』=================//
-global.namebot = '❋ཻུ۪۪⸙Darkbotz-MD⳹ ❋ཻུ۪۪'
-global.gc = 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
-global.web = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = 'https://instagram.com/Jarotr_'
+global.namebot = '❋۪WA BOT'
+global.gc = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.web = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.webm = 'https://instagram.com/x_pramaya_x'
 global.pulsa = '085850539404'
-global.nomer = 'https://wa.me/6285850539404' //ganti jadi nomor mu
+global.nomer = 'https://wa.me/94770611075' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERBULAN Rp 15.000\nMAKSIMAL 2 GRUP\n》2 BULAN 25.000\nMAKSIMAL 3 GRUP \n》3 BULAN Rp 40.000\nMAKSIMAL 2 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -44,10 +44,10 @@ global.fotonya8 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //foto ses
 global.donasi = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '❋ཻུ۪۪⸙Dark-MD⳹ ❋ཻུ۪۪'
+global.wm = '❋ཻུ۪۪⸙©️x_ᴘʀᴀᴍᴀʏᴀ_x'
 global.watermark = wm
-global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜ𝐉𝐚𝐫𝐨𝐭_𝙊𝙛𝙛cོ1꧅'
-global.wm3 = 'ᯤ 𝐉𝐚𝐫𝐨𝐭 𝙾𝙵𝙲'
+global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜ©️x_ᴘʀᴀᴍᴀʏᴀ_x꧅'
+global.wm3 = 'ᯤ PRAMAYA 𝙾𝙵𝙲'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
